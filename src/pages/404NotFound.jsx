2ddaf46f27a6a2fa2bc89f09e7cbcd865c27e96f
Mohit-patil0009 <mohitpatil0009@gmail.com>
2026-02-02 18:41:57 +0530

    not found page add

---
+++ src/pages/404NotFound.jsx
@@ -0,0 +1,7 @@
+export function NotFound() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="flex justify-center items-center">404 Page NotFound</h1>
+    </div>
+  );
+}
